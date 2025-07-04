4a17fad078d0afda6b3bdfb979c919c42e1dfd22
kimhyeon-seo <doubled0514@gmail.com>
2025-07-04 11:59:29 +0900

    f

--- doubled/backend/temp.py
+++ doubled/backend/temp.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify
 import pymysql
-
+from datetime import datetime 
 app = Flask(__name__)
-
 def get_connection():
     return pymysql.connect(
         host='localhost',
@@ -20,10 +19,6 @@ def receive_status():
     temperature = data.get('temperature')
     fan = data.get('fan')
     heater = data.get('heater')
-
-    if temperature is None or fan is None or heater is None:
-        return jsonify({"status": "failed", "reason": "필수 값 누락"}), 400
-
     conn = get_connection()
     try:
         with conn.cursor() as cursor:
@@ -32,41 +27,33 @@ def receive_status():
                 (temperature, fan, heater)
             )
         conn.commit()
-        return jsonify({"status": "success"}), 200
-    finally:
-        conn.close()
-
-# 2. 서버가 저장한 설정 온도 가져오기 
+        return {"status": "success"}
+    
 @app.route('/setting', methods=['GET'])
 def get_setting():
     conn = get_connection()
     try:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT temperature FROM setting ORDER BY id DESC LIMIT 1")
+            # 가장 최근 온도 1개만 가져오기
+            cursor.execute("SELECT temperature FROM setting ORDER BY datetime DESC LIMIT 1")
             row = cursor.fetchone()
             temp = row['temperature']
             if temp == -1:
-                return jsonify({
+                return {
                     "temperature": -1,
                     "fan": False,
                     "heater": False
-                }), 200
+                }
             else:
-                return jsonify({
+                return {
                     "temperature": temp
-                }), 200
-    finally:
-        conn.close()
+                }
 
 #3. 사용자가 온도 설정하기 
 @app.route('/setting', methods=['POST'])
 def set_setting():
     data = request.get_json()
     temperature = data.get('temperature')
-
-    if temperature is None:
-        return jsonify({"status": "failed", "reason": "temperature 값 없음"}), 400
-
     conn = get_connection()
     try:
         with conn.cursor() as cursor:
@@ -75,12 +62,10 @@ def set_setting():
                 (temperature,)
             )
         conn.commit()
-        return jsonify({
+        return {
             "status": "success",
             "temperature": temperature
-        }), 200
-    finally:
-        conn.close()
+        }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+app.run(debug=True,host = '0.0.0.0', port = 5000)
